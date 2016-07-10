59ecab5e6e80eb834fe30ef906bebf7adf4f9e1d
Keven <527030163@qq.com>
2016-07-10 21:58:53 +0800

    Add deviceProxy

---
+++ ayuep/app/src/main/java/com/ws/apple/ayuep/proxy/DeviceProxy.java
@@ -0,0 +1,37 @@
+package com.ws.apple.ayuep.proxy;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.ws.apple.ayuep.AYuePApplication;
+import com.ws.apple.ayuep.BuildConfig;
+import com.ws.apple.ayuep.handler.BaseAsyncHttpResponseHandler;
+import com.ws.apple.ayuep.model.DeviceModel;
+import com.ws.apple.ayuep.util.HttpHeaderUtil;
+import com.ws.apple.ayuep.util.HttpUtil;
+
+import cz.msebera.android.httpclient.Header;
+
+public class DeviceProxy {
+
+    public void registerDevice(Context context) {
+        String url = BuildConfig.SERVICE_URL + "/api/device";
+
+        DeviceModel device = new DeviceModel();
+        device.setDeviceIdentity(HttpHeaderUtil.getDeviceIdentity());
+        device.setDeviceType("Android");
+        Gson gson = new Gson();
+
+        HttpUtil.post(context, url, gson.toJson(device), new DeviceAsynHttpResonseHandler());
+    }
+
+    private class DeviceAsynHttpResonseHandler extends  BaseAsyncHttpResponseHandler {
+
+        @Override
+        public void onSuccess(String response) {
+            Log.d("Keven", "success");
+        }
+    }
+}
